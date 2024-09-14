30e5166f2015aaca9f13fcb82d76fb2a952ddce2
sbecker11 <sbecker11@users.noreply.github.com>
2024-09-14 02:58:04 +0000

    deploy: 282ffc62683dafe77934e577474a32118c65c76c

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+//
+// child/webpack.config.js
+// output.publicPath: '/workspace-rolodex-dist/'
+// devServer.static.publicPath: '/workspace-rolodex-dist/'
+// devServer.port: 9001
+// devServer.proxy.target: 'http://localhost:5001'
+// 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');  // Import webpack to use the HMR plugin
+
+module.exports = {
+  mode: 'development', // Set the mode to 'development' or 'production'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.[contenthash].js',
+    publicPath: '/workspace-rolodex-dist/',  // Update this to match your repository name
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',  // Ensure this path is correct
+      inject: 'body',
+      scriptLoading: 'defer'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'favicon.ico', to: 'favicon.ico' },
+        { from: 'media', to: 'media' }  // Ensure this path is correct
+      ],
+    }),
+    new webpack.HotModuleReplacementPlugin(),  // Add the HMR plugin
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'dist'),
+      publicPath: '/workspace-rolodex-dist/',  // Ensure this matches the output publicPath
+    },
+    compress: true,
+    port: 9001,
+    open: true,  // Automatically open the browser
+    hot: true, // Enable Hot Module Replacement
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:5001',  // The backend server URL
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }  // Remove /api prefix when forwarding
+      }
+    ]
+  },
+};
